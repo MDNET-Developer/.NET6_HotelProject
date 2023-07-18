543bb65eb3f2225be54fbd4797da02c538c592eb
MDNET-Developer <qarabagli.murad@mail.ru>
2023-07-19 01:27:41 +0400

    Add Main Website and provided partial view

---
+++ Frontend/MyHotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyHotelProject.WebUI.ViewComponents.Default
+{
+    public class _TeamPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        { 
+            return View(); 
+        }
+    }
+}
---
+++ Frontend/MyHotelProject.WebUI/ViewComponents/Default/_VideoPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyHotelProject.WebUI.ViewComponents.Default
+{
+    public class _VideoPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
